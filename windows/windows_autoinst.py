ef92144ce78cf4544ed54e575cd726dd368b3d7a
Conrad Jones <cjones@turboit.co.uk>
2019-11-28 23:35:02 +0000

    cleanup floppy creation temporary files on scope exit

--- windows/windows_autoinst.py
+++ windows/windows_autoinst.py
@@ -1,5 +1,6 @@
 import os
 import fabric
+from util import util
 
 def winGetBootFloppyFolder():
     script_dir = os.path.dirname(__file__)
@@ -27,19 +28,29 @@ class WindowsAutoInst:
         file.write(xml_text)
         return os.path.join(temp_location, "bootstrap.ps1")
 
-    def winCreate(self, location, name, pingback_ip):
+    def winCreateFloppy(self, location, name, pingback_ip):
+        with util.cleanup() as create_floppy_cleanup:
+            floppy_image_name = os.path.join(location, name + 'floppy.img')
+            remote_temp_location = os.path.join(location, name + '-temp')
+
+            bootstrap_filename = self._prepareBootstrap(name, pingback_ip)
+            create_floppy_cleanup.add(lambda: os.rmdir(os.path.dirname(bootstrap_filename)))
+            create_floppy_cleanup.add(lambda: os.unlink(bootstrap_filename))
+            self._conn.run('mkdir %s' % remote_temp_location)
+            create_floppy_cleanup.add(lambda: self._conn.run('rm -rf %s' % remote_temp_location))
+
+            self._conn.put(winGetBootFloppyFileName('autounattend.xml'), remote_temp_location)
+            self._conn.put(winGetBootFloppyFileName('bootstrap.cmd'), remote_temp_location)
+            self._conn.put(bootstrap_filename, remote_temp_location)
+            self._conn.run('dd if=/dev/zero of=%s count=1440 bs=1k' % floppy_image_name, hide='both')
+            self._conn.run('mkfs.msdos %s' % floppy_image_name, hide='out')
+            self._conn.run('mcopy -i %s %s/* ::/' % (floppy_image_name, remote_temp_location))
+            self._conn.run('chgrp kvm %s' % floppy_image_name)
+            self._conn.run('chmod g+w %s' % floppy_image_name)
+
+            return floppy_image_name
+
+    def winDeleteFloppy(self, location, name):
         floppy_image_name = os.path.join(location, name + 'floppy.img')
-        remote_temp_location = os.path.join(location, name + '-temp')
-
-        bootstrap_filename = self._prepareBootstrap(name, pingback_ip)
-        self._conn.run('mkdir %s' % remote_temp_location)
-        self._conn.put(winGetBootFloppyFileName('autounattend.xml'), remote_temp_location)
-        self._conn.put(winGetBootFloppyFileName('bootstrap.cmd'), remote_temp_location)
-        self._conn.put(bootstrap_filename, remote_temp_location)
-        self._conn.run('dd if=/dev/zero of=%s count=1440 bs=1k' % floppy_image_name, hide='both')
-        self._conn.run('mkfs.msdos %s' % floppy_image_name, hide='out')
-        self._conn.run('mcopy -i %s %s/* ::/' % (floppy_image_name, remote_temp_location))
-        self._conn.run('chgrp kvm %s' % floppy_image_name)
-        self._conn.run('chmod g+w %s' % floppy_image_name)
-
-        return floppy_image_name
+        print("winDeleteFloppy:%s" % floppy_image_name)
+        self._conn.run('rm %s' % floppy_image_name)
