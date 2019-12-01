e6b5d12f5233b4b69f2356e3f5420d7328998824
Conrad Jones <cjones@turboit.co.uk>
2019-12-01 23:46:11 +0000

    use ipconfig to get ip address.

--- windows/boot_floppy/bootstrap.ps1
+++ windows/boot_floppy/bootstrap.ps1
@@ -1,3 +1,3 @@
-$LocalIP = netsh interface ip show  addresses "Ethernet0" | Select-String "IP Address"
-$LocalIP = [regex]::Match($LocalIP, 'IP Address:.*\b([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})').Groups[1].Value
+$LocalIP = ipconfig | Select-String "IPv4 Address"
+$LocalIP = [regex]::Match($LocalIP, '([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})').Groups[1].Value
 Invoke-WebRequest -UseBasicParsing -Uri "${PINGBACK_URL}/pingback/$LocalIP" -Verbose
