d6f38de0bf24c5cbc2398e2f36fbe0bf4eb6a460
Conrad Jones <cjones@turboit.co.uk>
2019-12-02 05:33:53 +0000

    parse package list and image source.
    provide abstraction for image source, currently only ISO

--- parser/config.py
+++ parser/config.py
@@ -1,7 +1,8 @@
 import lxml.etree
 import lxml.html
-import uuid
 from shell import local
+import getpass
+from workflows.windows import baseimage
 
 def get_hypervisor(hypervisor_type, *, shell):
     if hypervisor_type == "Fusion":
@@ -10,6 +11,7 @@ def get_hypervisor(hypervisor_type, *, shell):
 
     raise Exception("Unknown hypervisor type:%s" % hypervisor_type)
 
+
 def get_diskvisor(diskvisor_type, *, shell):
     if diskvisor_type == "VDiskManager":
         from vm import vmware_vdiskmanager_backend
@@ -17,10 +19,12 @@ def get_diskvisor(diskvisor_type, *, shell):
 
     raise Exception("Unknown diskvisor type:%s" % diskvisor_type)
 
+
 def parse_diskvisor_type(xml):
     disk_s = xml.xpath(".//Disk")
     type_s = disk_s[0].xpath(".//Type")
     return type_s[0].text
+
 
 def parse_hypervisor_type(xml):
     hypervisor_s = xml.xpath(".//Hypervisor")
@@ -33,6 +37,7 @@ def parse_hypervisor_type(xml):
 
     return type_s[0].text
 
+
 def get_shell(xml):
     shell_s = xml.xpath(".//Shell")
     if len(shell_s) == 0 or shell_s[0].text == 'Local':
@@ -40,16 +45,53 @@ def get_shell(xml):
 
     raise Exception("Unknown shell type")
 
-def parse_image(xml):
+
+def parse_image_source(xml):
     image_source_s = xml.xpath(".//Image/Source")
     if len(image_source_s) is 0:
         raise Exception("No Image/Source")
 
     iso_s = xml.xpath(".//ISO")
     if len(iso_s) > 0:
-        iso = iso_s[0]
+        return IsoSource(iso_s[0].text)
 
     raise Exception("Requires valid image source")
+
+
+def parse_packages(xml):
+    package_s = xml.xpath(".//Image/Packages/Package")
+    packages = []
+    for package_node in package_s:
+        packages += package_node.text
+
+def parse_admin_user(xml):
+    password_s = xml.xpath(".//Image/Administrator/Password")
+    if len(password_s) is 0:
+        raise Exception("Requires Image/Administrator/Password node")
+
+    if password_s[0].text == '*':
+        password = None
+        while password is None:
+            print("Enter password:")
+            pass1 = getpass.getpass()
+
+            print("Confirm password:")
+            pass2 = getpass.getpass()
+
+            if pass1 == pass2:
+                password = pass1
+            else:
+                print("Passwords did not match")
+
+        return password
+
+    return password_s[0].text
+
+class IsoSource:
+
+    def __init__(self, iso):
+        self._iso = iso
+
 
 def parse_config(file_name):
     # print(raw_xml)
@@ -62,4 +104,10 @@ def parse_config(file_name):
     diskvisor_type = parse_diskvisor_type(xml)
     diskvisor = get_diskvisor(diskvisor_type, shell=shell)
 
+    image_source = parse_image_source(xml)
+    package_list = parse_packages(xml)
 
+    admin_password = parse_admin_user(xml)
+
+    baseimage.build_windows_base_image(hypervisor, diskvisor_type, windows_autoinst, winrs_remote, disk_location,
+                                              40, iso, iso_drivers, packages, keep_vm=False)
