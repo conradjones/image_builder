d2cbaa03520fb5082cd21cb1206a13ff07452d4c
Conrad Jones <cjones@turboit.co.uk>
2019-12-12 15:02:36 +0000

    return true from shell_steps - each step should return true or false otherwise an exception is raised

--- steps/shell_steps.py
+++ steps/shell_steps.py
@@ -1,7 +1,7 @@
 from steps import step_utils
 
 
-def ssh_shell(*, address):
+def ssh_shell(steps_state, *, address):
     from shell import ssh
     return ssh.SSH(conn_string=address)
 
@@ -15,3 +15,4 @@ def step_shell(step_data, steps_state):
     step_utils.check_step_values(step_data, ['type'])
 
     steps_state.shells[step_data['name']] = step_utils.execute_map_step_type(shell_types, step_data, steps_state)
+    return True
