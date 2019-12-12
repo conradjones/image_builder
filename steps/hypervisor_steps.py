137588d8c5acf79fbbd4873f6a33152d5c6a38d2
Conrad Jones <cjones@turboit.co.uk>
2019-12-12 19:16:13 +0000

    add fusion hypervisor to hypervisor steps

--- steps/hypervisor_steps.py
+++ steps/hypervisor_steps.py
@@ -6,12 +6,22 @@ def libvirt_hypervisor(steps_state, *, address):
     return libvirt_backend.LibVirtBackEnd(conn_string=address)
 
 
+def fusion_hypervisor(steps_state, *, shell):
+    from vm import vmware_run_backend
+    shell_obj = steps_state.get_shell(shell)
+    return vmware_run_backend.VMwareVMRunBackend(shell=shell_obj)
+
+
 hypervisor_types = {
-    "libvirt": libvirt_hypervisor
+    "libvirt": libvirt_hypervisor,
+    "fusion": fusion_hypervisor
 }
 
 
 def step_hypervisor(step_data, steps_state):
     step_utils.check_step_values(step_data, ['type'])
 
-    steps_state[step_data['name']] = step_utils.execute_map_step_type(hypervisor_types, step_data, steps_state)
+    steps_state.hypervisors[step_data['name']] = step_utils.execute_map_step_type(hypervisor_types, step_data,
+                                                                                  steps_state)
+
+    return True
