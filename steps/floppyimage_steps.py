4d1bacbd749c33ca570e2d0d345885f4e18c8c0a
Conrad Jones <cjones@turboit.co.uk>
2019-12-12 21:57:23 +0000

    fix shell and return value in floppy step

--- steps/floppyimage_steps.py
+++ steps/floppyimage_steps.py
@@ -6,7 +6,7 @@ from util import util
 def windows_auto_install_floppyimage(steps_state, *, ping_back=True, administrator_password, shell, file, group=None,
                                      perms=None):
     from windows import windows_autoinst
-    shell_obj = steps_state.shells[shell]
+    shell_obj = steps_state.get_shell(shell)
 
     unattend = windows_autoinst.WindowsAutoInst(shell=shell_obj, location=os.path.dirname(file),
                                                 admin_password=administrator_password, group=group,
@@ -21,7 +21,8 @@ floppyimage_types = {
 
 
 def step_floppyimage(step_data, steps_state):
-    step_utils.check_step_values(step_data, ['type'])
+    step_utils.check_step_values(step_data, ['type', 'name'])
 
-    steps_state.disks[step_data['floppy_images']] = step_utils.execute_map_step_type(floppyimage_types, step_data,
+    steps_state.disks[step_data['name']] = step_utils.execute_map_step_type(floppyimage_types, step_data,
                                                                                      steps_state)
+    return True
